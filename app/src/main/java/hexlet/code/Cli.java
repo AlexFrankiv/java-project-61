dc325bfda2ae94599d51888d4025dbd15e36b019
franAlex <frankivs@mail.ru>
2025-09-14 16:52:07 +0500

    fix checkstyle problem.

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 import java.util.Scanner;
 public class Cli {
-    public static String greetingWithPlayer(){
+    public static String greetingWithPlayer() {
         Scanner sc = new Scanner(System.in);
         System.out.println("May i have your name? ");
         String answerPlayer = sc.nextLine();

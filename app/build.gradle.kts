10c72c3ca9c8cf373091326ad17f5732a8c63064
franAlex <frankivs@mail.ru>
2025-10-02 17:06:40 +0500

    delete Test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,15 +23,7 @@ sonar {
 checkstyle {
     toolVersion = "10.12.4"
 }
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
 
-tasks.test {
-    useJUnitPlatform()
-}
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
